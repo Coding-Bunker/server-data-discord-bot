b1cf06e4ad09711f508dc297208008854658c1fc
SimoSbara <38192614+SimoSbara@users.noreply.github.com>
2021-06-03 13:29:52 +0200

    Modifica algoritmo
    - tolta dipendenza dotenv
    - aggiunto ciclo per mandare un messaggio a discord ogni 5 secondi
    (test heroku)

--- discordbot.py
+++ discordbot.py
@@ -1,30 +1,39 @@
 import discord
 from discord.ext import commands
 import requests
-from dotenv import dotenv_values
+#from dotenv import dotenv_values
 import os
+import time
+
+def current_milli_time():
+    return round(time.time() * 1000)
 
 #caricamento varibili d'ambiente
-config = dotenv_values(".env")
+#config = dotenv_values(".env")
 
 #permessi per poter effettuare qualsiasi azione
 client = commands.Bot(command_prefix=">", intents = discord.Intents.all())
 
 @client.event
 async def on_ready():
-        
+
+    time = current_milli_time()
+    
     sum = 0
 
     channel = client.get_channel(int(os.environ['CHANNEL_ID']))
     guild = client.get_guild(int(os.environ['GUILD_ID']))
 
-    #esclude i bot dalla conta dei membri
-    members = await guild.fetch_members().flatten()
-    for m in members:
-        if not m.bot:
-            sum += 1
-                
-    await channel.send('Il numero di utenti è ' + str(sum))
+    while(True):
+        if(current_milli_time() - time) >= 5000:
+            #esclude i bot dalla conta dei membri
+            members = await guild.fetch_members().flatten()
+            for m in members:
+                if not m.bot:
+                    sum += 1
+                    
+            await channel.send('Il numero di utenti è ' + str(sum))
+            time = current_milli_time()
     
 @client.event
 async def on_message(message):
